f9e9ae88dd80412f04653767f7458d763195ae14
신혜승 <hyeseung2000@gmail.com>
2022-05-25 04:26:36 +0900

    [fix] 위도 경도 수정

--- src/main/java/com/lookatthis/flora/service/UserService.java
+++ src/main/java/com/lookatthis/flora/service/UserService.java
@@ -123,8 +123,8 @@ public class UserService {
     public void updateAddress(User user, String userAddress, Point userPoint) {
 
         user.setUserAddress(userAddress);
-        user.setUserLatitude(userPoint.getX());
-        user.setUserLongitude(userPoint.getY());
+        user.setUserLatitude(userPoint.getY());
+        user.setUserLongitude(userPoint.getX());
         user.setUserPoint(userPoint);
     }
 
